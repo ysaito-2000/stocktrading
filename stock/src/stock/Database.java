ef7398860fa5c85fa094785fb13df4b07930bf67
Joel Jacob <joel.jacob2k@outlook.com>
2021-05-09 03:54:39 -0700

    Updated User.java and Database.java

--- stock/src/stock/Database.java
+++ stock/src/stock/Database.java
@@ -4,11 +4,13 @@ import java.util.*;
 import java.io.*;
 
 public class Database {
+    private static Set<User> userSet = new HashSet<>();
+    private static Set<Stock> stockSet = new HashSet<>();
 
     public ArrayList<DataPoint> getData() throws IOException
     {
-        ArrayList<DataPoint> values = new ArrayList<DataPoint>();
-        File input = new File("data/Apple.txt");
+        ArrayList<DataPoint> values = new ArrayList<>();
+        File input = new File("stock/data/Apple.txt");
         FileReader fr = new FileReader(input);
         BufferedReader br = new BufferedReader(fr);
         try
@@ -42,7 +44,7 @@ public class Database {
      */
     public void addStock(Stock s)
     {
-
+        stockSet.add(s);
     }
 
     public void updateStock()
@@ -52,11 +54,15 @@ public class Database {
 
     public void addUser(User u)
     {
-
+        userSet.add(u);
     }
 
     public void updateUser(User u)
     {
 
+    }
+
+    public boolean verifyUser(User u) {
+        return userSet.contains(u);
     }
 }
